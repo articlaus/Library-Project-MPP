a7e4faad27b00f778ae50525cff8053911d292a6
arti_tj@yahoo.com <!CyALlW3>
2018-09-11 14:22:45 -0500

    added new window and updated the main class

--- Library/src/Main.java
+++ Library/src/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,7 +11,10 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ui/MemberAddWindow.fxml"));
+        primaryStage.setTitle("Testing");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Group project for MPP Course of MUM 2018 August Entry
 Group #6
 
 Members
+
 -Dawit Habte Woldergiorgis
+
 -Ganbat Bayar
+
 -Tuy Tam Phan
