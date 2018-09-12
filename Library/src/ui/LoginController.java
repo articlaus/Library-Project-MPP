1cd05db424ed8e268afb46fd023474fc55513c19
dawit <dawit@172.19.144.216>
2018-09-11 23:58:40 -0500

    add login page, login controller and main menu page

---
+++ Library/src/ui/LoginController.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class LoginController {
+	
+	@FXML Button loginButton;
+	@FXML TextField usernameField;
+	@FXML PasswordField passwordField;
+	@FXML Label loginErrorMessage;
+	
+	@FXML
+	public void clickLogin (ActionEvent event) {
+		
+		// ensure username field is not blank
+		if (usernameField.getText() == null || usernameField.getText().equals(""))
+			loginErrorMessage.setText("Username should not be blank!");
+		else
+			loginErrorMessage.setText("The username password combination is not correct (Unimplemented login)");
+		
+	}
+	
+}
