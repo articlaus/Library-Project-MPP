c7b7edbbae9ec732baf4139903759531cea1aa42
Dawit Woldegiorgis <dwoldegiorgis@mum.edu>
2018-09-13 10:19:35 -0500

    changed import

--- Library/src/Main.java
+++ Library/src/Main.java
@@ -1,9 +1,7 @@
 import dataaccess.DataStorageFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.*;
 
 public class Main extends Application {
 
